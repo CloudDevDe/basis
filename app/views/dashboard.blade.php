2f49b596cd2592c7a414f77b3f5f647cdabdc19b
CloudDev <info@clouddev.de>
2014-12-20 18:50:40 +0100

    Grundstruktur angelegt
    Die Grundstruktur für die gesamte App.

---
+++ app/views/dashboard.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@section ('title')
+    Dashboard
+@stop
+
+@section ('description')
+<!-- Placeholder -->
+@stop
+
+@section('content')
+
+    <div class="box-body">
+  
+@stop
