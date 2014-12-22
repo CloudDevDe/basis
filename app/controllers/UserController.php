a3605b54abcb1a826a2dc005c5c1ca23ab379a15
CloudDev <info@clouddev.de>
2014-12-22 11:38:23 +0100

    Benutzer erstellen
    Es ist jetzt möglich neue Benutzer zu erstellen

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -12,4 +12,40 @@ class UserController extends \BaseController {
             ->with('users', $users);
     }
 
+    public function create()
+    {
+        // load the create form (app/views/nerds/create.blade.php)
+        return View::make('users.create');
+    }
+
+    public function store()
+    {
+        // validate
+        // read more on validation at http://laravel.com/docs/validation
+        $rules = array(
+            'username'       => 'required',
+            'email'      => 'required|email',
+        );
+        $validator = Validator::make(Input::all(), $rules);
+
+        // process the login
+        if ($validator->fails()) {
+            return Redirect::to('users/create')
+                ->withErrors($validator)
+                ->withInput(Input::except('password'));
+        } else {
+            // store
+            $user = new User;
+            $user->username       = Input::get('username');
+            $user->email      = Input::get('email');
+            $user->save();
+
+            // redirect
+            Session::flash('message', 'Successfully created nerd!');
+            return Redirect::to('users');
+        }
+    }
+
+
+
 }
